d8c73d39fd784289e9b4aade9be05825ea8ebd17
Maseh911 <ac121658@avcol.school.nz>
2024-07-01 13:28:31 +1200

    DummyData #3

--- MedicalAppointingSystem/Areas/Identity/Data/Dummydata.cs
+++ MedicalAppointingSystem/Areas/Identity/Data/Dummydata.cs
@@ -18,12 +18,38 @@ namespace MedicalAppointingSystem.Data
                 }
                 var Patients = new Patient[]
                 {
-                    new Patient { FirstName = "Maseh", LastName = "Essa", Phone = "0240489487", Email = "ac121658@avcol.school.nz", Address = "Avondale College"},
+                    new Patient { FirstName = "Maseh", LastName = "Essa", Phone = "0240489487", Email = "ac121658@avcol.school.nz", Address = "Avondale College", DoctorId = 1, DiagnosisId = 1},
                 };
                 Context.Patient.AddRange(Patients);
                 Context.SaveChanges();
 
+                var Doctors = new Doctor[]
+                {
+                    new Doctor { FirstName = "Homer", LastName = "Simpson", Phone = "02184739203", Email = "HomerSimpson@gmail.com"},
+                };
+                Context.Doctor.AddRange(Doctors);
+                Context.SaveChanges();
 
+                var AppointmentTimes = new AppointmentTime[]
+                {
+                    new AppointmentTime { AppointedTime = new DateTime(2024, 7, 23, 9, 15, 0), PatientId = 2},
+                };
+                Context.AppointmentTime.AddRange(AppointmentTimes);
+                Context.SaveChanges();
+
+                var Hospitals = new Hospital[]
+                {
+                    new Hospital { HospitalName = "Auckland Hospital", Address = "Auckland"}
+                };
+                Context.Hospital.AddRange(Hospitals);
+                Context.SaveChanges();
+
+                var Diagnosis = new Diagnosis[]
+                {
+                    new Diagnosis { Diagnosis_Name = "ADHD"}
+                };
+                Context.Diagnosis.AddRange(Diagnosis);
+                Context.SaveChanges();
             }
         }
     }
